76f5eeec0a9c8b13be171b8ef6cb58ce2b17dbc5
Ryan Beiden <rdbeiden@gmail.com>
2020-08-29 14:06:05 -0500

    add reading of all birds seen in firebase with styling I still want to add a `source map` of what all the colors stand for.

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import {
+  CardColumns,
+} from 'reactstrap';
 
+import birbData from '../../../helpers/data/birbData';
+import authData from '../../../helpers/data/authData';
 import BirbCard from '../../shared/BirbCard/BirbCard';
 
+import './Home.scss';
+
 class Home extends React.Component {
+  state = {
+    birbs: [],
+  }
+
+  getBirbs = () => {
+    birbData.getBirbsByUid(authData.getUid())
+      .then((birbs) => this.setState({ birbs }))
+      .catch((err) => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getBirbs();
+  }
+
   // editBirbEvent = (e) => {
   //   e.preventDefault();
   //   const birbId = 'birb10239';
@@ -10,9 +31,15 @@ class Home extends React.Component {
   // }
 
   render() {
+    const { birbs } = this.state;
+
+    const getBirbCards = birbs.map((birb) => <BirbCard key={birb.id} birb={birb}/>);
+
     return (
       <div className="Home">
-        <BirbCard />
+        <CardColumns>
+          {getBirbCards}
+        </CardColumns>
       </div>
     );
   }
--- src/components/shared/BirbCard/BirbCard.js
+++ src/components/shared/BirbCard/BirbCard.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from 'reactstrap';
+// import { Link } from 'react-router-dom';
 
-import birbData from '../../../helpers/data/birbData';
+import birbShape from '../../../helpers/props/birbShape';
+import './BirbCard.scss';
 
 class BirbCard extends React.Component {
+  static propTypes = {
+    birb: birbShape.birbShape,
+  }
+
   render() {
+    const { birb } = this.props;
+
     return (
       <div className="BirbCard">
-        <Link to='/birbs/{}'><h3>Birb!</h3></Link>
+        <Card className="m-4 card-container">
+          <CardBody className="card-body">
+            <CardTitle className="type">{birb.type}</CardTitle>
+            <CardSubtitle className="location">{birb.location}</CardSubtitle>
+            <CardSubtitle className="seenAt">{birb.seenAt}</CardSubtitle>
+            <CardSubtitle className="size">{birb.size}</CardSubtitle>
+            <CardSubtitle className="wasSleeping">{birb.wasSleeping ? <i className="fas fa-bed"></i> : <i className="fas fa-eye"></i>}</CardSubtitle>
+            {birb.notes.length > 0 ? <CardText className="notes"><em>{birb.notes}</em></CardText> : ''}
+            <Button className="view-bird-button">View Bird</Button>
+          </CardBody>
+        </Card>
       </div>
     );
   }
