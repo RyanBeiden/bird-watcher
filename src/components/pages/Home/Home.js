cf6942f219bbbd292e0d4439363ea988f016fa94
Ryan Beiden <rdbeiden@gmail.com>
2020-08-29 11:08:53 -0500

    add Link to Home component

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   editBirbEvent = (e) => {
@@ -12,6 +13,8 @@ class Home extends React.Component {
       <div className="Home">
         <h2>Home</h2>
         <button className="btn btn-dark" onClick={this.editBirbEvent}>Edit Birb</button>
+        <Link to="/new">New Birb</Link>
+        <Link to="/birbs/birb9865654">Specific Birb</Link>
       </div>
     );
   }
