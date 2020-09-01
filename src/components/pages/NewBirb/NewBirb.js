b4e3f577f7ed16d1eb9ccd99f5b8e3b228e2c52c
Ryan Beiden <rdbeiden@gmail.com>
2020-08-31 21:31:19 -0500

    update styling to be more consistent

--- src/components/pages/NewBirb/NewBirb.js
+++ src/components/pages/NewBirb/NewBirb.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 class NewBirb extends React.Component {
-  componentDidMount() {
-    document.getElementById('home').style.backgroundColor = '';
-    document.getElementById('newBirb').style.backgroundColor = '#9EE09E';
-  }
-
   render() {
     return (
       <h2>New Birb</h2>
